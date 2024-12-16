67dd20866a511286d2045726b0fd998e75d56589
Marco Visani <92217074+mvisani@users.noreply.github.com>
2024-12-16 10:27:42 +0100

    added new files

---
+++ cfm-code/Message.h
@@ -0,0 +1,84 @@
+/*#########################################################################
+# Mass Spec Prediction and Identification of Metabolites
+#
+# message.h
+#
+# Description: 	Utility class for storing and passing around sparse
+#               messages.
+#
+# Copyright (c) 2013, Felicity Allen
+# All rights reserved.
+
+# This file is part of the cfm-id project.
+# The contents are covered by the terms of the GNU Lesser General Public
+# License, which is included in the file license.txt, found at the root
+# of the cfm source tree.
+#########################################################################*/
+
+#ifndef __MESSAGE_H__
+#define __MESSAGE_H__
+#include "Util.h"
+
+#include <boost/numeric/ublas/vector_sparse.hpp>
+
+// Log Domain Double (converts null entry to -A_BIG_DBL)
+class logdbl {
+	double value;
+
+public:
+	explicit logdbl(double f = -A_BIG_DBL) { value = f; }
+
+	logdbl &operator=(double f) {
+		value = f;
+		return *this;
+	}
+
+	explicit operator double() { return value; }
+
+	explicit operator double() const { return value; }
+};
+
+// Normalized sparse vector in the log domain
+class Message {
+
+public:
+	explicit Message(unsigned int size = 0);
+
+	double getIdx(unsigned int i);
+
+	void addToIdx(unsigned int i, double value);
+
+	void multIdx(unsigned int i, double value);
+
+	void reset(unsigned int size);
+
+	unsigned int isEmpty() { return empty; }
+
+	Message &operator=(const Message &rhs);
+
+	void addWeightedMessage(Message &msg, double weight);
+
+	typedef boost::numeric::ublas::mapped_vector<logdbl>::iterator iterator;
+	typedef boost::numeric::ublas::mapped_vector<logdbl>::const_iterator const_iterator;
+
+	iterator begin() {
+		if (!normalized) normalize();
+		return message.begin();
+	}
+
+	iterator end() { return message.end(); }
+
+	unsigned int size() { return message.size(); }
+
+	void print();
+
+private:
+	double log_sum;
+	boost::numeric::ublas::mapped_vector<logdbl> message;
+	unsigned int empty;
+	unsigned int normalized;
+
+	void normalize();
+};
+
+#endif // __MESSAGE_H__
